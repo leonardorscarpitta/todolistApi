370065671f19216c035520d2d59bb6f3f6c87bae
Leonardo <leonardorscarpitta@gmail.com>
2024-07-22 14:58:13 -0300

    feat: add program packages (controller, model, repository & service)

---
+++ src/main/java/me/leonardors/todolist/model/Task.java
@@ -0,0 +1,40 @@
+package me.leonardors.todolist.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String description;
+    private boolean status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/me/leonardors/todolist/service/TaskService.java
@@ -0,0 +1,31 @@
+package me.leonardors.todolist.service;
+
+import me.leonardors.todolist.model.Task;
+import me.leonardors.todolist.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> findAll() {
+        return taskRepository.findAll();
+    }
+
+    public Task save(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task update(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
